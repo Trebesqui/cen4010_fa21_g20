09e57b4f2f3923407c2add90dce0d56c786182d2
sreardon2018 <89849594+sreardon2018@users.noreply.github.com>
2021-11-12 01:27:56 -0500

    Updated About Page In Order To Display Profile Images And Redirect to Personal About Me When Clicked

--- striker_prototype/about.php
+++ striker_prototype/about.php
@@ -8,22 +8,26 @@ include("header.php");
                         <div class="col-lg-8 col-xxl-6">
                             <div class="text-center my-5">
                                 <h1 class="fw-bolder mb-3">Striker</h1>
-                                <p class="lead fw-normal text-muted mb-4">A platform on which any journalist, blogger, or writer can post articles. Readers will have to pay a small fee to get behind the paywall of each article. This solves many problems facing journalism, text media, and the user experience.</p>
+                                <p class="lead fw-normal text-muted mb-4">A platform on which any journalist, blogger, or writer 
+                                    can post articles. Readers will have to pay a small fee to be able to reach each article. This 
+                                    solves many problems facing journalism, text media, and the user experience.</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </header>
-            <!-- Team members section-->
+            <!-- Team Members Section-->
             <section class="py-5 bg-light">
                 <div class="container px-5 my-5">
                     <div class="text-center">
                         <h2 class="fw-bolder">Our team</h2>
-                        <p class="lead fw-normal text-muted mb-5">Dedicated to quality and your success</p>
+                        <p class="lead fw-normal text-muted mb-5">Click on our pictures to find out more about us!</p>
                     </div>
                     <div class="col mb-5 mb-5 mb-xl-0">
                         <div class="text-center">
-                            <img class="img-fluid rounded-circle mb-4 px-4" src="https://dummyimage.com/150x150/ced4da/6c757d" alt="..." />
+                            <a href="https://lamp.cse.fau.edu/~ngiacobbe2020/Nicholas/" target="_blank">
+                                <img class="img-fluid rounded-circle mb-4 px-4" src="assets/nicholas_profile.png" alt="..." />
+                            </a>
                             <h5 class="fw-bolder">Nicholas Giacobbe</h5>
                             <div class="fst-italic text-muted">Team Leader<br> Front &amp; Back End Developer </div>
                         </div>
@@ -31,28 +35,36 @@ include("header.php");
                     <div class="row gx-5 row-cols-1 row-cols-sm-2 row-cols-xl-4 justify-content-center">
                         <div class="col mb-5 mb-5 mb-xl-0">
                             <div class="text-center">
-                                <img class="img-fluid rounded-circle mb-4 px-4" src="https://dummyimage.com/150x150/ced4da/6c757d" alt="..." />
+                                <a href="https://lamp.cse.fau.edu/~alexrodrigue2018" target="_blank">
+                                    <img class="img-fluid rounded-circle mb-4 px-4" src="assets/alex_profile.png" alt="..." />
+                                </a>
                                 <h5 class="fw-bolder">Alex Rodriguez</h5>
                                 <div class="fst-italic text-muted">Front End Lead <br> Group Lamp Server Admin</div>
                             </div>
                         </div>
                         <div class="col mb-5 mb-5 mb-sm-0">
                             <div class="text-center">
-                                <img class="img-fluid rounded-circle mb-4 px-4" src="https://dummyimage.com/150x150/ced4da/6c757d" alt="..." />
+                                <a href="https://lamp.cse.fau.edu/~ltrebesqui2018/about_me/" target="_blank">
+                                    <img class="img-fluid rounded-circle mb-4 px-4" src="assets/luiza_profile.png" alt="..." />
+                                </a>
                                 <h5 class="fw-bolder">Luiza Trebesqui</h5>
                                 <div class="fst-italic text-muted">Back End Lead <br> Github Master</div>
                             </div>
                         </div>
                         <div class="col mb-5">
                             <div class="text-center">
-                                <img class="img-fluid rounded-circle mb-4 px-4" src="https://dummyimage.com/150x150/ced4da/6c757d" alt="..." />
+                                <a href="https://lamp.cse.fau.edu/~sreardon2018/Prin-SE/Shay/index.html" target="_blank">
+                                    <img class="img-fluid rounded-circle mb-4 px-4" src="assets/shay_profile.png" alt="..." />
+                                </a>
                                 <h5 class="fw-bolder">Shay Reardon</h5>
                                 <div class="fst-italic text-muted">Scrum Master <br> Front &amp; Back End Developer</div>
                             </div>
                         </div>
                         <div class="col mb-5">
                             <div class="text-center">
-                                <img class="img-fluid rounded-circle mb-4 px-4" src="https://dummyimage.com/150x150/ced4da/6c757d" alt="..." />
+                                <a href="https://lamp.cse.fau.edu/~truongnguyen2019/" target="_blank">
+                                    <img class="img-fluid rounded-circle mb-4 px-4" src="assets/truong_profile.png" alt="..." />
+                                </a>
                                 <h5 class="fw-bolder">Truong Nguygen</h5>
                                 <div class="fst-italic text-muted">MySQL Admin <br> Front &amp; Back End Developer</div>
                             </div>
@@ -65,7 +77,7 @@ include("header.php");
         <footer class="bg-dark py-4 mt-auto">
             <div class="container px-5">
                 <div class="row align-items-center justify-content-between flex-column flex-sm-row">
-                    <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; Your Website 2021</div></div>
+                    <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; Striker 2021</div></div>
                     <div class="col-auto">
                         <a class="link-light small" href="#!">Privacy</a>
                         <span class="text-white mx-1">&middot;</span>
