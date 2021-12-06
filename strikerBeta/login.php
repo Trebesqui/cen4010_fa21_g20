d05b4b96c7fc6640c474e2576b539c2f9828800b
giacobn <56774667+giacobn@users.noreply.github.com>
2021-12-06 17:47:58 -0500

    minor updates

--- strikerBeta/login.php
+++ strikerBeta/login.php
@@ -9,7 +9,7 @@ include_once('include/config.php');
                 <form action="login.php" method="POST" class="form-container text-center">
                     <?php include('errors.php'); ?>
                     <br><h1>Welcome to Striker</h1> <br>
-                    <label for="email"><b>Email</b></label><br>
+                    <label for="email"><b>Username</b></label><br>
                     <input type="text" placeholder="Enter Username" name="username" required><br><br>
 
                     <label for="password"><b>Password</b></label><br>
